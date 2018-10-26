739f3e263434fd64d31e972ac4d0ff0413901010
olimpiogilberto <olimpiogilberto@hotmail.com>
2018-10-26 09:47:01 -0300

    alterado pada o DELL
    o projeto foi alterado para funcionar no DELL, não sei se funcionará no Lenovo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+import { AlertaService } from './alerta.service';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    ListaPessoaComponent,
+    NgIfComponent,
+    NgSwitchCaseComponent,
+    NgForComponent,
+    NgClassComponent
   ],
+ 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AlertaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
